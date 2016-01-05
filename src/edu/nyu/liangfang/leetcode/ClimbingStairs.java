b8a0bfd4a53b4b55ab8ed379f873b2d56367323c
LiangFang <liangfang09@gmail.com>
2016-01-04 22:31:18 -0800

    update

--- src/edu/nyu/liangfang/leetcode/ClimbingStairs.java
+++ src/edu/nyu/liangfang/leetcode/ClimbingStairs.java
@@ -1,22 +1,42 @@
 package edu.nyu.liangfang.leetcode;
 
 public class ClimbingStairs {
-	public int climbStairs(int n) {
-		if (n <= 0)
-			return 0;
-        int[] arr = new int[n + 1];		// Because we will consider 0 to n, so n + 1 numbers in total
-        return climbStairs(n, arr);
+	// iterative solution
+    public int climbStairs_iterative(int n) {
+        if (n <= 0) return 0;
+        
+        int[] map = new int[n + 1];
+        map[0] = 1;
+        map[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            map[i] = map[i - 1] + map[i - 2];
+        }
+        return map[n];
+    }
+
+    // recursion solution
+    public int climbStairs_recursion(int n) {
+		if (n <= 0) 
+            return 0;
+        
+        int[] map = new int[n + 1];
+        map[0] = 1;
+        map[1] = 1;
+        return res(n, map);
     }
     
-    public int climbStairs(int n, int[] arr) {
-        if (n < 0)				//Corner condition
-            return 0;
-        else if (n == 0)		// Because when we consider n as 2, then either climb from 1 or climb form 0, each counts as 1
-            return 1;
-        else if (arr[n] > 0)
-            return arr[n];
-        int num = climbStairs(n-1, arr) + climbStairs(n-2, arr);
-        arr[n] = num;
-        return num;
+    private int res(int n, int[] map) {
+        if (map[n] > 0) 
+            return map[n];
+        int r = res(n - 1, map) + res(n - 2, map);
+        map[n] = r;
+        return r;
+    }
+
+
+    // Simple recursion solution - no dynamic programming
+    public int climbStairs_simple(int n) {
+        if (n == 0 || n == 1) return 1;
+        return climbStairs(n - 1) + climbStairs(n - 2);
     }
 }
---
+++ src/edu/nyu/liangfang/leetcode/containsDuplicate.java
@@ -0,0 +1,25 @@
+public class containsDuplicate {
+	// Set solution
+    public boolean containsDuplicate_set(int[] nums) {
+        if (nums == null || nums.length == 0) return false;
+        
+        Set<Integer> set = new HashSet<Integer>();
+        for (int i : nums) {
+            if (set.contains(i)) return true;
+            set.add(i);
+        }
+        return false;
+    }
+
+    // Sort solution
+    public boolean containsDuplicate_sort(int[] nums) {
+        if (nums == null || nums.length == 0) return false;
+        
+        Arrays.sort(nums);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1])
+                return true;
+        }
+        return false;
+    }
+}

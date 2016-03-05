dfd65db7390c27e137282a8de7f91455de0c9391
Liang Fang <liangfang@yahoo-inc.com>
2016-03-04 16:24:03 -0800

    add more

---
+++ src/edu/nyu/liangfang/leetcode/findTheDuplicateNumber.java
@@ -0,0 +1,20 @@
+public class findTheDuplicateNumber {
+    // method 1: similar to linked list cycle II
+    public int findDuplicate(int[] nums) {
+        int slow = 0, fast = 0;
+        while (true) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if (slow == fast) break;
+        }
+        
+        slow = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+
+    
+}
---
+++ src/edu/nyu/liangfang/leetcode/searchA2DMatrix2.java
@@ -0,0 +1,16 @@
+public class Solution {
+    // O(m + n) solution
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix.length == 0) return false;
+        
+        int r = 0, c = matrix[0].length - 1;
+        while (r < matrix.length && c >= 0) {
+            if (matrix[r][c] == target) return true;
+            else if (matrix[r][c] < target) r++;
+            else c--;
+        }
+        return false;
+    }
+
+    // O () solution
+}
